44acacdb8c78ac803b36b38e07a5e7e2b640629d
Ankita <ghadge.ankita197@gmail.com>
2026-08-18 21:18:25 +0530

    annotate serial version id with @serial

--- url-shortener-backend/src/main/java/com/url/shrotener/service/UserDetailsImpl.java
+++ url-shortener-backend/src/main/java/com/url/shrotener/service/UserDetailsImpl.java
@@ -7,15 +7,17 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serial;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDetailsImpl implements UserDetails  {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private Long id;
