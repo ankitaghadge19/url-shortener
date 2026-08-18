c580bd78e1b474d9a63652af4917c8ab220becb6
Ankita <ghadge.ankita197@gmail.com>
2026-08-18 10:42:19 +0530

    initialize the project

---
+++ url-shortener-backend/src/main/java/com/url/shrotener/UrlShrotenerApplication.java
@@ -0,0 +1,12 @@
+package com.url.shrotener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShrotenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShrotenerApplication.class, args);
+    }
+}
---
+++ url-shortener-backend/src/test/java/com/url/shrotener/UrlShrotenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.shrotener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShrotenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
