16ca71014bed3eecb623cb62400637da09761408
Ankita <ghadge.ankita197@gmail.com>
2026-08-18 12:34:41 +0530

    configure spring security

---
+++ url-shortener-backend/src/main/java/com/url/shrotener/security/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.url.shrotener.security;
+
+import com.url.shrotener.security.jwt.JwtAuthenticationFilter;
+import com.url.shrotener.service.UserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
+public class WebSecurityConfig {
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider(userDetailsServiceImpl);
+        daoAuthProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/urls/**").authenticated()
+                                .requestMatchers("/{shortUrl}").permitAll()
+                                .anyRequest().authenticated()
+                );
+        httpSecurity.authenticationProvider(authenticationProvider());
+        httpSecurity.addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
+        return httpSecurity.build();
+    }
+}
